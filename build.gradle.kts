7b7a68b6f6b84103e42be9fb6fc49a79f759d55c
User <pohzhixian@gmail.com>
2023-11-01 14:59:09 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.king9487"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.github.king9487.Main"
+}
 
 repositories {
     mavenCentral()
@@ -13,6 +18,19 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
